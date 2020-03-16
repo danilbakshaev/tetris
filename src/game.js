8391f79ab26e25c54bb5e04416d04b1ff0f1a955
danilbakshaev <48173055+danilbakshaev@users.noreply.github.com>
2020-03-16 20:23:25 +0500

    Add rotate piece

--- src/game.js
+++ src/game.js
@@ -58,8 +58,41 @@ export default class Game {
         }
     }
 
+    rotatePiece() {
+        this.rotateBlocks();
+
+        if (this.hasCollision()){
+            this.rotateBlocks(false);
+        }
+    }
+
+    rotateBlocks(clockwise = true) {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = i; j < y - i; j++) {
+                const temp = blocks[i][j];
+
+                if (clockwise) {
+                    blocks[i][j] = blocks[y - j][i];
+                    blocks[y - j][i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[j][y - i];
+                    blocks[j][y - j] = temp;
+                } else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }
+            }
+        }
+    }
+
     hasCollision() {
-        const { y: pieceY, x: pieceX, blocks} = this.activePiece;
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
@@ -77,7 +110,7 @@ export default class Game {
     }
 
     lockPiece(){
-        const { y: pieceY, x: pieceX, blocks} = this.activePiece;
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
