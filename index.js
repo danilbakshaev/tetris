7b26140ce097c150fb9fecec65fdadcedd51d41f
danilbakshaev <48173055+danilbakshaev@users.noreply.github.com>
2020-05-20 01:10:46 +0500

    Добавил визуальную часть

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
+
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37: //left arrow
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 38: //up arrow
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 39: //right arrow
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 40: //down arrow
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
